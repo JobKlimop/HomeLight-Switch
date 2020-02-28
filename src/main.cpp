9e8f2b8c61ec216e1ac83552328e84f4282e73ec
Unknown <thomas_641991@hotmail.com>
2020-02-28 20:33:10 +0100

    Added methods & endpoints

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 #include <WiFi.h>
-// #include <WebServer.h>
 #include <ESPAsyncWebServer.h>
 #include <NeoPixelBus.h>
 #include <AsyncJson.h>
-// #include <ArduinoJson.h>
+#include <ArduinoJson.h>
 
-#define EEPROM_SIZE 1
+#define EEPROM_SIZE 512
 
 const int powerStateAddress = 0;
-const int groupNumberAddress = 1;
+const int groupNumberAddress = 2;
 
 const uint16_t pixelCount = 82;
 const uint pixelPin = 17;
@@ -23,10 +22,10 @@ AsyncWebServer server(80);
 int powerState;
 int groupNumber;
 int colorSaturation = 255;
-int greenValue = 0;
-int redValue = 0;
-int blueValue = 0;
-int whiteValue = 0;
+int greenValue;
+int redValue;
+int blueValue;
+int whiteValue;
 
 NeoPixelBus<NeoGrbwFeature, NeoEsp32I2s1800KbpsMethod> strip(pixelCount, pixelPin);
 RgbwColor green(colorSaturation, 0, 0, 0);
@@ -37,23 +36,60 @@ RgbwColor black(0);
 
 void setGroupNumber(DynamicJsonDocument json) {
   groupNumber = json["groupNumber"];
-  Serial.print("!");
-  Serial.print(groupNumber);
-  Serial.println("!");
-  EEPROM.put(groupNumberAddress, groupNumber);
-  Serial.println(EEPROM.commit());
-  if(EEPROM.commit() == false) {
-    Serial.println("False");
-  } else {
-    Serial.println("True");
-  }
+  EEPROM.write(groupNumberAddress, groupNumber);
   EEPROM.commit();
 }
 
-DynamicJsonDocument parseData(AsyncWebServerRequest *request, uint8_t *data, String endpoint) {
-  DynamicJsonDocument doc(2048);
+void setColor() {
+  for(uint16_t pixel = 0; pixel < pixelCount; pixel++) {
+    strip.SetPixelColor(pixel, RgbwColor(greenValue, redValue, blueValue, whiteValue));
+  }
+}
+
+void setColorValues(DynamicJsonDocument json) {
+  JsonArray colorValues = json["colorValues"];
+
+  if(greenValue != colorValues[0]) {
+    greenValue = colorValues[0];
+  }
+  if(redValue != colorValues[1]) {
+    redValue = colorValues[1];
+  }
+  if(blueValue != colorValues[2]) {
+    blueValue = colorValues[2];
+  }
+  if(whiteValue != colorValues[3]) {
+    whiteValue = colorValues[3];
+  }
+
+  setColor();
+}
+
+void setBrightness(DynamicJsonDocument json) {
+  colorSaturation = json["brightness"];
+  setColor();
+}
+
+void setPowerState(DynamicJsonDocument json) {
+  if(json["powerState"] == "HIGH") {
+    digitalWrite(relayPin, HIGH);
+    EEPROM.write(powerStateAddress, HIGH);
+    EEPROM.commit();
+    powerState = HIGH;
+    setColorValues(json);
+  } else if(json["powerState"] == "LOW") {
+    digitalWrite(relayPin, LOW);
+    EEPROM.write(powerStateAddress, LOW);
+    EEPROM.commit();
+    powerState = LOW;
+  }
+}
+
+DynamicJsonDocument parseData(AsyncWebServerRequest *request, uint8_t *data, size_t jsonCapacity, String endpoint) {
+  DynamicJsonDocument doc(jsonCapacity);
   DeserializationError err = deserializeJson(doc, data);
 
+  Serial.println(serializeJsonPretty(doc, Serial));
   if(err) {
     request->send(400, "text/plain", "err on" + endpoint);
   } else {
@@ -77,10 +113,12 @@ void setup() {
     }
   } else {
     powerState = LOW;
-    EEPROM.put(powerStateAddress, powerState);
+    EEPROM.write(powerStateAddress, powerState);
     EEPROM.commit();
   }
+  if(EEPROM.read(groupNumberAddress) != 255) {
     groupNumber = EEPROM.read(groupNumberAddress);
+  }
 
   WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED) {
@@ -90,10 +128,25 @@ void setup() {
 
   Serial.println(WiFi.localIP());
 
+  server.on("/controller/setPowerState", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, 
+  [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+    size_t jsonCapacity = JSON_OBJECT_SIZE(1);
+    DynamicJsonDocument doc = parseData(request, data, jsonCapacity, "/setPowerState");
+    setPowerState(doc);
+  });
+
   server.on("/controller/setGroup", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, 
   [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-      DynamicJsonDocument doc = parseData(request, data, "/setGroup");
+    size_t jsonCapacity = JSON_OBJECT_SIZE(1);
+      DynamicJsonDocument doc = parseData(request, data, jsonCapacity, "/setGroup");
       setGroupNumber(doc);
+  });
+
+  server.on("/controller/setColor", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, 
+  [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+    size_t jsonCapacity = JSON_OBJECT_SIZE(1) + JSON_ARRAY_SIZE(4);
+    DynamicJsonDocument doc = parseData(request, data, jsonCapacity, "/setColor");
+    setColorValues(doc);
   });
 
   server.begin();
@@ -101,9 +154,19 @@ void setup() {
 
 void loop() {
   Serial.println(powerState);
-  Serial.println(EEPROM.get(powerStateAddress, powerState));
+  Serial.println(EEPROM.read(powerStateAddress));
   Serial.println(groupNumber);
-  Serial.println(EEPROM.get(groupNumberAddress, groupNumber));
+  Serial.println(EEPROM.read(groupNumberAddress));
+  Serial.print("Brightness: ");
+  Serial.println(colorSaturation);
+  Serial.print("green: ");
+  Serial.println(greenValue);
+  Serial.print("red: ");
+  Serial.println(redValue);
+  Serial.print("blue: ");
+  Serial.println(blueValue);
+  Serial.print("white: ");
+  Serial.println(whiteValue);
   Serial.println("---------");
   delay(2000);
 }
